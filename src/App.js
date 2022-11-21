c12aed982a2169ace54c9eaa586dd787403560bb
SumanFarhan <sumanfarhan2800@gmail.com>
2022-11-21 17:00:00 +0500

    input field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [obj, setObj] = useState({
+    name: "",
+    age: "",
+    email: ""
+  })
+ 
+  // const [age,setAge]=useState("")
+  // const [email,setEmail]=useState("")
+  const add = (event) => {
+    const {name,value}  = event.target;
+    setObj((preValue) => {
+      console.log(preValue)
+      return{
+        ...preValue,
+        [name]:value
+      }
+    })
+  }
+  // const addName=(event)=>{setName(event.target.value)}
+  // const addAge=(event)=>{setAge(event.target.value)}
+  // const addEmail=(event)=>{setEmail(event.target.value)
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <form>
+        <label>Name:
+          <input type="text" value={obj.name} name="name" onChange={add} />
+        </label>
+        <label>Age:
+          <input type="text" value={obj.age} name="age" onChange={add} />
+        </label>
+        <label>Email:
+          <input type="email" value={obj.email} name="email" onChange={add} />
+        </label>
+      </form>
+      <button type="submit">Submit</button>
+      <ol>
+        <li>{obj.name}</li>
+        <li>{obj.age}</li>
+        <li>{obj.email}</li>
+      </ol>
+    </>
+  )
 }
 
-export default App;
+export default App
